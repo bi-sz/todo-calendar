5656bd2ee4e1a1df326e572f2464608aad42b387
kwonbi <kwonbi@pumpkinev.co.kr>
2023-10-26 14:16:05 +0900

    수정

--- App.js
+++ App.js
@@ -1,11 +1,158 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import dayjs from 'dayjs';
+import { SimpleLineIcons } from '@expo/vector-icons'; 
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+
+import Margin from './src/Margin';
+import { runPracticeDayjs } from './src/practice-dayjs';
+import { getCalendarColumns, getDayColor, getDayText } from './src/util';
+import { useCalendar } from './src/hook/use-calendar';
+import { useTodoList } from './src/hook/use-todo-list';
+
+const columnSize = 35;
+
+const statusBarHeight = getStatusBarHeight(true);
+const bottomSpace = getBottomSpace();
+
+const Column = ({
+  text,
+  color,
+  opacity,
+  disabled,
+  onPress,
+  isSelected,
+}) => {
+  return (
+    <TouchableOpacity 
+      disabled = {disabled}
+      onPress = { onPress }
+      style={{ 
+        width: columnSize, 
+        height: columnSize, 
+        justifyContent: "center", 
+        alignItems: "center",
+        backgroundColor: isSelected ? "#c2c2c2" : "transparent",
+        borderRadius: columnSize / 2,
+        }}>
+      <Text style={{ color, opacity }}>{text}</Text>
+    </TouchableOpacity>
+  )
+}
+const ArrowButton = ({ iconName, onPress }) => {
+  return (
+    <TouchableOpacity onPress={ onPress } style={{ paddingHorizontal: 20, paddingVertical: 15 }}>
+            <SimpleLineIcons name={ iconName } size={15} color="#404040" />
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
+  const now = dayjs();
+  const {
+    selectedDate,
+    setSelectedDate,
+    isDatePickerVisible,
+    showDatePicker,
+    hideDatePicker,
+    handleConfirm,
+    subtract1Month,
+    add1Month,
+  } = useCalendar(now);
+  const {} = useTodoList(selectedDate);
+
+  const columns = getCalendarColumns(selectedDate);
+
+  const onPressLeftArrow = subtract1Month
+  const onPressRightArrow = add1Month
+
+  const ListHeaderComponent = () => {
+    const currentDateText = dayjs(selectedDate).format("YYYY.MM.DD.");
+    return (
+      <View>
+        {/* < YYYY.MM.DD. > */}
+        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+          <ArrowButton iconName="arrow-left" onPress={onPressLeftArrow} />
+
+          <TouchableOpacity onPress={showDatePicker}>
+            <Text style={{ fontSize: 20, color: "#404040" }}>{currentDateText}</Text>
+          </TouchableOpacity>
+
+          <ArrowButton iconName="arrow-right" onPress={onPressRightArrow} />
+
+        </View>
+
+        {/* 일 ~ 토 */}
+        <View style={{ flexDirection: "row" }}>
+          {[0, 1, 2, 3, 4, 5, 6].map(day => {
+            const dayText = getDayText(day);
+            const color = getDayColor(day);
+            return (
+              <Column 
+                key={`day-${day}`} 
+                text={dayText} 
+                color={color} 
+                opacity={1} 
+                disabled={true}
+              />
+            )
+          })}
+        </View>
+      </View>
+    )
+  }
+  
+  const renderItem = ({ item: date }) => {
+    const dateText = dayjs(date).get('date');
+    const day = dayjs(date).get('day');
+    const color = getDayColor(day);
+    const isCurrentMonth = dayjs(date).isSame(selectedDate, 'month');
+    const onPress = () => {
+      setSelectedDate(date);
+    }
+    const isSelected = dayjs(date).isSame(selectedDate, 'date');
+    return (
+      <Column 
+        text={dateText} 
+        color={color} 
+        opacity={isCurrentMonth ? 1 : 0.4}
+        onPress={onPress}
+        isSelected={isSelected}
+      />
+    )
+  }
+  useEffect(() => {
+     runPracticeDayjs();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Image
+        source={{
+          uri:fdgdfgdfg
+        }}
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute"
+        }}
+      />
+
+      <FlatList
+        data={columns}
+        keyExtractor={(_, index) => `column-${index}`}
+        numColumns={7}
+        renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
+      />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
     </View>
   );
 }
@@ -16,5 +163,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop : statusBarHeight,
   },
 });
